341e34a182fe14c39b6638214ef3aadcca4b039f
alloyking1 <alloyking1@gmail.com>
2026-04-04 02:16:18 +0100

    fixed vector search syntax

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,45 +4,47 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use MongoDB\BSON\ObjectId;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index($id)
     {
-        // 1. Get the current post
+
+        // 1. Get current post
         $post = Post::findOrFail($id);
 
-        // 2. Run vector search
-        $results = Post::raw(function ($collection) use ($post) {
-            return $collection->aggregate([
-                [
-                    '$vectorSearch' => [
-                        'index' => 'vector_index', // your index name
-                        'queryVector' => $post->embedding,
-                        'path' => 'embedding',
-                        'numCandidates' => 100,
-                        'limit' => 5
-                    ]
-                ],
-                [
-                    '$match' => [
-                        '_id' => [
-                            '$ne' => new ObjectId($post->_id)
-                        ]
-                    ]
-                ]
-            ]);
-        });
+        // 2. Access native MongoDB collection
+        $collection = DB::connection('mongodb')
+            ->getMongoDB()
+            ->selectCollection('posts');
 
-        // 3. Format recommendations
-        $recommendations = collect($results)->map(function ($item) {
-            return [
+        // 3. Run vector search (RAW MongoDB QUERY)
+        $cursor = $collection->aggregate([
+            [
+                '$vectorSearch' => [
+                    'index' => 'vector_index',
+                    'queryVector' => $post->embedding,
+                    'path' => 'embedding',
+                    'numCandidates' => 100,
+                    'limit' => 5,
+                ]
+            ]
+        ]);
+
+        // 4. Convert to Laravel collection
+        $results = collect($cursor->toArray());
+
+        // 5. Remove current post + format
+        $recommendations = $results
+            ->filter(fn ($item) => (string) $item['_id'] !== (string) $post->_id)
+            ->map(fn ($item) => [
                 'id' => (string) $item['_id'],
                 'title' => $item['title'],
-            ];
-        });
-    
-        // 4. Return structured response
+            ])
+            ->values();
+
+        // 6. Return response
         return response()->json([
             'post' => [
                 'title' => $post->title,
@@ -50,5 +52,6 @@ class PostController extends Controller
             ],
             'recommendations' => $recommendations,
         ]);
+
     }
 }
